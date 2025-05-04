4734513c648b48c7055b4d36cf6f0eccf1d03ca0
Ayush <sinhaayush1125@gmail.com>
2025-05-04 18:15:24 +0530

    Changed base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const LOGO = "https://i.imgur.com/TcH1Lge.png";
-export const BASE_URL = "http://localhost:7777";
+export const BASE_URL = "/api";
 export const LANDING_PAGE_IMG =
   "https://images.pexels.com/photos/3184325/pexels-photo-3184325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
