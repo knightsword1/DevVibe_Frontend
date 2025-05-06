7d123702158d6dc5ae35eb10b4cf554e5942f1d0
Ayush <sinhaayush1125@gmail.com>
2025-05-06 15:23:38 +0530

    made base_url dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
 export const LOGO = "https://i.imgur.com/TcH1Lge.png";
-export const BASE_URL = "/api";
 export const LANDING_PAGE_IMG =
   "https://images.pexels.com/photos/3184325/pexels-photo-3184325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
